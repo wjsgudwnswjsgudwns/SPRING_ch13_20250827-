0ad94b837079d40b25df71a790444c96fe12f125
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-08-27 11:03:05 +0900

    @RequestParam

--- src/main/java/com/jhj/formtest/MemberController.java
+++ src/main/java/com/jhj/formtest/MemberController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
@@ -17,11 +18,35 @@ public class MemberController {
 		return "login";
 	}
 	
+	@RequestMapping(value = "/login2")
+	public String login2() {
+		
+		return "login2";
+	}
+	
 	@RequestMapping(value = "/loginOk")
 	public String loginOk(HttpServletRequest request, Model model, HttpSession session, HttpServletResponse response) {
 		
 		String mid = request.getParameter("mid");
 		String mpw = request.getParameter("mpw");
+		
+		model.addAttribute("inputId", mid);
+		
+		if(mid.equals("tiger") && mpw.equals("12345")) {
+			session.setAttribute("sessionId", mid);
+			return "loginOk";
+		} else { // 로그인 실패
+			return "loginFail";
+		}
+	}
+	
+	@RequestMapping(value = "/loginOk2")
+	public String loginOk2(@RequestParam("mid") String mid, @RequestParam("mpw") String mpw, Model model, HttpSession session, HttpServletResponse response) {
+		//@RequestParam("mid") String mid
+		//String mid = request.getParameter("mid");
+		
+		//@RequestParam("mpw") String mpw
+		//String mpw = request.getParameter("mpw");
 		
 		model.addAttribute("inputId", mid);
 		
